09f6108755d4855a6eacc6102143fca6c838900f
Akash Kriplani <aakash23kriplani@gmail.com>
2021-05-18 01:01:49 +0530

    MEAN: Add spinner, routes in BE and CRUD operation APIs

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
-
 const app = express();
+
+const postRoutes = require('./routes/posts');
 
 mongoose.connect('mongodb+srv://akash:Vvyoke1cBgNrxOoL@cluster0.hofn1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log('Connected to MongoDB successfully!')
@@ -18,33 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({ message: 'Post added successfully.', postId: createdPost._id });
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: 'Posts fetched successfully.',
-      posts: documents
-    });
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    res.status(200).json({ message: 'Post deleted successfully.' });
-  })
-});
+app.use('/api/posts', postRoutes);
 
 module.exports = app;
