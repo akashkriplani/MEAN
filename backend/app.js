6da20363a5759b93aa2f85d1132279e6c028356e
Akash Kriplani <aakash23kriplani@gmail.com>
2021-05-20 02:11:59 +0530

    MEAN: Upload image file paths on server

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -14,6 +15,7 @@ mongoose.connect('mongodb+srv://akash:Vvyoke1cBgNrxOoL@cluster0.hofn1.mongodb.ne
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,15 +1,48 @@
 const express = require('express');
 const router = express.Router();
+// For file uploads, multer is a package that helps express
+const multer = require('multer');
 
 const Post = require('../models/post');
 
-router.post('', (req, res, next) => {
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValidMimeType = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid mime type');
+    if (isValidMimeType) {
+      error = null;
+    }
+    // Path provided should be relative to server.js file
+    cb(error, 'backend/images');
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const fileExt = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + fileExt);
+  }
+});
+
+router.post('', multer({ storage: fileStorage }).single('image'), (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host');
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: url + '/images/' + req.file.filename
   });
   post.save().then((createdPost) => {
-    res.status(201).json({ message: 'Post added successfully.', postId: createdPost._id });
+    res.status(201).json({
+      message: 'Post added successfully.',
+      post: {
+        ...createdPost,
+        id: createdPost._id
+      }
+    });
   });
 });
 
@@ -28,11 +61,17 @@ router.delete('/:id', (req, res, next) => {
   })
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', multer({ storage: fileStorage }).single('image'), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + '://' + req.get('host');
+    imagePath = url + '/images/' + req.file.filename;
+  }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: imagePath
   });
   Post.updateOne({ _id: req.params.id }, post).then(() => {
     res.status(200).json({ message: 'Post updated successfully.' });
