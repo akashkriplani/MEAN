245e690cd7371e1b870e823665460b14b156ff50
Akash Kriplani <aakash23kriplani@gmail.com>
2021-05-26 00:26:13 +0530

    MEAN: Auto login user on page refresh

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -15,6 +15,21 @@ export class AuthService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
+  autoAuthUser(): void {
+    const authInformation = this.getAuthData();
+    if (!authInformation) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+    }
+  }
+
   createUser(email: string, password: string) {
     const authData: AuthData = { email, password };
     return this.http.post(`http://localhost:3000/api/user/signup`, authData)
@@ -40,10 +55,11 @@ export class AuthService {
         this.token = token;
         if (this.token) {
           const expiresInDuration = response.expiresIn;
-          this.tokenTimer = setTimeout(() => {
-            this.logout();
-          }, expiresInDuration * 1000);
+          this.setAuthTimer(expiresInDuration);
           this.isAuthenticated = true;
+          const now = new Date();;
+          const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+          this.saveAuthData(token, expirationDate);
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
         }
@@ -55,6 +71,36 @@ export class AuthService {
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     clearTimeout(this.tokenTimer);
+    this.clearAuthData();
     this.router.navigate(['/']);
   }
+
+  private clearAuthData(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+  }
+
+  private getAuthData(): { token: string, expirationDate: Date } | null {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate)
+    };
+  }
+
+  private setAuthTimer(duration: number): void {
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
+  }
+
+  private saveAuthData(token: string, expirationDate: Date): void {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
 }
