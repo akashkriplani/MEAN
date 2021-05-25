0657f0aed4edc3590001d148a2456040ec060b36
Akash Kriplani <aakash23kriplani@gmail.com>
2021-05-25 23:59:50 +0530

    MEAN: Add expiration duration for auth token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { Observable, Subject } from 'rxjs';
 export class AuthService {
   private isAuthenticated = false;
   private token: string;
+  private tokenTimer: any;
   private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) { }
@@ -33,11 +34,15 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email, password };
-    return this.http.post<{ token: string }>(`http://localhost:3000/api/user/login`, authData)
+    return this.http.post<{ token: string, expiresIn: number }>(`http://localhost:3000/api/user/login`, authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (this.token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
@@ -49,6 +54,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
   }
 }
