bcc3a00bbab7daaf308995f22ef56852e9e8dc84
Akash Kriplani <aakash23kriplani@gmail.com>
2021-05-30 01:41:40 +0530

    MEAN: Add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthModule } from './auth/auth.module';
 import { PostModule } from './posts/post.module';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent
   ],
   imports: [
@@ -29,6 +26,7 @@ import { ErrorComponent } from './error/error.component';
     FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    AuthModule,
     PostModule
   ],
   providers: [
